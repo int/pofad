367500d2a1463302e897db87c9fdd32a03853377
Alecs King <alecsk@gmail.com>
2010-11-21 23:48:57 +0800

    first cut of ch5: a quick & dirty brute parsec

---
+++ ch5_century/century.hs
@@ -0,0 +1,38 @@
+import Text.Parsec
+import Text.Parsec.Token
+import Text.Parsec.Expr
+import Text.Parsec.Language
+import Data.Char
+
+-- this is my quick & dirty version
+-- get all arrangments -> parsec -> filter
+make :: [Integer] -> [String]
+make [] = []
+make [x] = [show x]
+make (x:xs) = concat [ [s ++ y, s ++ " + " ++ y, s ++ "*" ++ y] | y <- make xs ]
+    where s = show x
+
+get :: String -> Integer
+get s = case parse expr "" s of
+             Right i -> i
+
+solutions :: Integer -> [Integer] -> [String]
+solutions ans = filter ((==ans) . get . strip) . make
+    where strip = filter (/= ' ')
+
+-- parser
+lexer = makeTokenParser emptyDef
+expr = buildExpressionParser table (decimal lexer)
+table = [[op "*" (*) AssocLeft], [op "+" (+) AssocLeft]]
+    where op s f = Infix (do {string s; return f})
+
+-- print helper
+doit ans digits = do
+    putStrLn $ "get " ++ show ans ++ " from " ++ show digits ++ ":\n"
+    mapM_ putStrLn $ solutions ans digits
+    putStrLn ""
+
+-- some examples
+main = do
+    doit 100 [1..9]
+    doit 1234 $ map (fromIntegral . digitToInt) $ take 12 . drop 2 $ show pi
